2cc3ad960746e06625db66e700849bf54977e6d6
dhong9 <danielhong35@gmail.com>
2023-05-05 20:50:34 -0700

    Adds CommentAdmin

--- portfolio/threadly/admin.py
+++ portfolio/threadly/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Comment
 
-# Register your models here.
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ('name', 'email', 'post', 'created', 'active')
+    list_filter = ('active', 'created', 'updated')
+    search_fields = ('name', 'email', 'body')
