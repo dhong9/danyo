98de7b0ebd70bbc9a8761fa1baeed42fd7549c8f
dhong9 <danielhong35@gmail.com>
2023-05-11 19:34:01 -0700

    Added max_length field to email

--- portfolio/threadly/models.py
+++ portfolio/threadly/models.py
@@ -24,7 +24,7 @@ class Comment(models.Model):
 # Contact model
 class Contact(models.Model):
     full_name = models.CharField(max_length=100)
-    email = models.EmailField()
+    email = models.EmailField(max_length=254)
     subject = models.CharField(max_length=100)
     query_txt = models.TextField()
     add_time = models.DateTimeField(auto_now_add=True)
