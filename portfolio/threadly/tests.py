43cf5801250c9342324548139a97a0aa49d6be6e
dhong9 <danielhong35@gmail.com>
2023-05-25 18:42:07 -0700

    Added test for Comment model

--- portfolio/threadly/tests.py
+++ portfolio/threadly/tests.py
@@ -1,3 +1,13 @@
 from django.test import TestCase
+from portfolio.threadly.models import Comment
 
 # Create your tests here.
+class CommentTest(TestCase):
+
+    def create_comment(self, pageName="sports", name="John Doe", email="john_doe@aol.com", body="Sports keeps you healthy."):
+        return Comment.objects.create(pageName=pageName, name=name, email=email, body=body)
+    
+    def test_comment_creation(self):
+        comment = self.create_comment()
+        self.assertTrue(isinstance(comment, Comment))
+        self.assertEqual(str(comment), comment.body)
