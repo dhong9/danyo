87277d0496b0097f63eb0ea7e49fc425a0e5ad84
dhong9 <danielhong35@gmail.com>
2023-05-25 21:23:21 -0700

    Added unit test for Contact model

--- portfolio/threadly/tests.py
+++ portfolio/threadly/tests.py
@@ -1,7 +1,6 @@
 from django.test import TestCase
-from portfolio.threadly.models import Comment
+from portfolio.threadly.models import Comment, Contact
 
-# Create your tests here.
 class CommentTest(TestCase):
 
     def create_comment(self, pageName="sports", name="John Doe", email="john_doe@aol.com", body="Sports keeps you healthy."):
@@ -11,3 +10,13 @@ class CommentTest(TestCase):
         comment = self.create_comment()
         self.assertTrue(isinstance(comment, Comment))
         self.assertEqual(str(comment), comment.body)
+
+class ContactTest(TestCase):
+
+    def create_contact(self, full_name="Gabe Real", email="gabriel@hotmail.com", subject="Things Are Real", query_txt="Yup, things just got real."):
+        return Contact.objects.create(full_name=full_name, email=email, subject=subject, query_txt=query_txt)
+    
+    def test_contact_creation(self):
+        contact = self.create_contact()
+        self.assertTrue(isinstance(contact, Contact))
+        self.assertEqual(str(contact), contact.query_txt)
