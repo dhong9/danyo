0ad2d18e81bba6570db952123e85a37a5f9aae78
dhong9 <danielhong35@gmail.com>
2023-06-03 17:48:17 -0700

    Added email port

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -195,6 +195,7 @@ CSRF_TRUSTED_ORIGINS = [
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = "dhong9.pythonanywhere.com"
+EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 DEFAULT_FROM_EMAIL = "danielhong35@gmail.com"
 SERVER_EMAIL = "danielhong35@gmail.com"
