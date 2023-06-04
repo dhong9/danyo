9824d2930e9b018f6390898041f7a48119728cbf
dhong9 <danielhong35@gmail.com>
2023-06-03 18:49:24 -0700

    Moved email password to config file

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -196,6 +196,6 @@ CSRF_TRUSTED_ORIGINS = [
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = "smtp.gmail.com"
 EMAIL_HOST_USER = "danielhong35@gmail.com"
-EMAIL_HOST_PASSWORD = "yourpassword"
+EMAIL_HOST_PASSWORD = config("EMAIL_PASSWORD")
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
