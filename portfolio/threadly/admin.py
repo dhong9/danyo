89855256db3570148ff9d604d7bc5b82a6fd6bda
dhong9 <danielhong35@gmail.com>
2023-07-29 08:02:16 -0700

    Added Contact model to admin

--- portfolio/threadly/admin.py
+++ portfolio/threadly/admin.py
@@ -1,8 +1,15 @@
 from django.contrib import admin
-from .models import Comment
+
+# Register your models here.
+from .models import Comment, Contact
 
 @admin.register(Comment)
 class CommentAdmin(admin.ModelAdmin):
     list_display = ('name', 'email', 'pageName', 'create', 'active', 'isPlainText')
     list_filter = ('active', 'create', 'updated')
     search_fields = ('name', 'email', 'body')
+
+@admin.register(Contact)
+class ContactAdmin(admin.ModelAdmin):
+    list_display = ('full_name', 'email', 'query_txt', 'add_time')
+    search_fields = ('full_name', 'email', 'query_txt', 'add_time')
