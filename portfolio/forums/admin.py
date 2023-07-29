dc6ee73f122bf419243fc35889bebddcf02c96ba
dhong9 <danielhong35@gmail.com>
2023-07-28 21:51:42 -0700

    Added forums Post to admin

--- portfolio/forums/admin.py
+++ portfolio/forums/admin.py
@@ -1,9 +1,15 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import Category
+from .models import Category, Post
 
 @admin.register(Category)
 class CategoryAdmin(admin.ModelAdmin):
     list_display = ('name', 'description')
     search_fields = ('name', 'description')
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('postName', 'name', 'email', 'body', 'isPlainText', 'create', 'updated', 'active', 'mainFeatured', 'featured')
+    list_filter = ('active', 'create', 'updated')
+    search_fields = ('postName', 'name', 'email', 'body')
