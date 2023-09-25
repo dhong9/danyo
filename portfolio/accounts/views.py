bceba40564a6d8003e06150b220392e76ecd929a
dhong9 <danielhong35@gmail.com>
2023-09-24 19:45:53 -0700

    Simplified UpdateView

--- portfolio/accounts/views.py
+++ portfolio/accounts/views.py
@@ -19,19 +19,10 @@ class RegisterView(generics.CreateAPIView):
     permission_classes = (AllowAny,)
     serializer_class = RegisterSerializer
 
-class UpdateView(generics.RetrieveUpdateAPIView):
-    permission_classes = (AllowAny,)
+class UpdateView(generics.ModelViewSet):
+    queryset = User.objects.all()
+    permission_classes = (IsAuthenticated,)
     serializer_class = RegisterSerializer
-
-    def retrieve(self, request, *args, **kwargs):
-        serializer = self.serializer_class(request.user)
-        return Response(serializer.data, status=status.HTTP_200_OK)
-    
-    def update(self, request, *args, **kwargs):
-        serializer = self.serializer_class(request.user, data=request.data, partial=True)
-        serializer.is_valid(raise_exception=True)
-        serializer.save()
-        return Response(serializer.data, status=status.HTTP_200_OK)
 
 @api_view(['GET'])
 def getRoutes(request):
