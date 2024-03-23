8bd828750442ba55f313ea1bb88fdab9336b3223
dhong9 <danielhong35@gmail.com>
2024-03-22 17:36:16 -0700

    Updated profiles url to get details by ID

--- portfolio/accounts/urls.py
+++ portfolio/accounts/urls.py
@@ -12,6 +12,6 @@ urlpatterns = [
     path('update/<pk>/', views.UpdateView.as_view(), name='auth_update'),
     path('delete/<pk>/', views.UpdateView.as_view(), name='auth_delete'),
     path('test/', views.testEndPoint, name='test'),
-    path('profiles/', views.ProfileViewSet.as_view({'get': 'list', 'post': 'create'}), name='profiles'),
+    path('profiles/<pk>', views.ProfileViewSet.as_view({'get': 'retrieve', 'put': 'update', 'patch': 'partial_update', 'delete': 'destroy'}), name='profiles'),
     path('', views.getRoutes)
 ]
