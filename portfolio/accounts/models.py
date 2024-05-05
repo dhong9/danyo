c5f23993159eb49d94eae90f812c28b258366d61
dhong9 <danielhong35@gmail.com>
2024-05-05 14:49:56 -0700

    Override user profile image save

--- portfolio/accounts/models.py
+++ portfolio/accounts/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
-
+from PIL import Image
 
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE) # Delete profile when user is deleted
@@ -8,6 +8,18 @@ class Profile(models.Model):
 
     def __str__(self):
         return f'{self.user.username} Profile' #show how we want it to be displayed
+
+    # Override the save method if the model
+    def save(self):
+        super().save()
+
+        img = Image.open(self.image.path) # Open image
+
+        # resize image
+        if img.height > 300 or img.width > 300:
+            output_size = (300, 300)
+            img.thumbnail(output_size) # Resize image
+            img.save(self.image.path) # Save it again and override the larger image
 
 User._meta.get_field('email')._unique = True
 User._meta.get_field('email').blank = False
