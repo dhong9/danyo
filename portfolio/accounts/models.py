ea44cc665f15bddfd4c73edc20cb0fe1eab848c8
dhong9 <danielhong35@gmail.com>
2024-05-13 18:05:42 -0700

    Imported receiver

--- portfolio/accounts/models.py
+++ portfolio/accounts/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.dispatch import receiver
 from django_rest_passwordreset.signals import reset_password_token_created
 from django.core.mail import send_mail, EmailMessage
 from PIL import Image
