87606398ff96abb2803c7d9fc42966b8f42b958e
dhong9 <danielhong35@gmail.com>
2024-05-13 18:21:27 -0700

    Added missing comma

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -48,7 +48,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'django_rest_passwordreset'
+    'django_rest_passwordreset',
     'corsheaders',
     'rest_framework',
     'portfolio.threadly',
