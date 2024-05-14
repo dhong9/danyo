34ffd88eb83d429ed6d95b42dc63eedcb40d2669
dhong9 <danielhong35@gmail.com>
2024-05-13 18:11:44 -0700

    Added django_rest_passwordreset to installed apps

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -48,6 +48,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django_rest_passwordreset'
     'corsheaders',
     'rest_framework',
     'portfolio.threadly',
