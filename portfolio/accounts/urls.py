e39c24be77d215f76e9cccc96bf955e8d1f9ffed
dhong9 <danielhong35@gmail.com>
2024-05-26 14:52:07 -0700

    Removed nonexistent URLs

--- portfolio/accounts/urls.py
+++ portfolio/accounts/urls.py
@@ -6,14 +6,6 @@ from rest_framework_simplejwt.views import (
 )
 
 urlpatterns = [
-    path('token/', views.MyTokenObtainPairView.as_view(), name='token_obtain_pair'),
-    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
-    path('register/', views.RegisterView.as_view(), name='auth_register'),
-    path('update/<pk>/', views.UpdateView.as_view(), name='auth_update'),
-    path('delete/<pk>/', views.UpdateView.as_view(), name='auth_delete'),
-    path('test/', views.testEndPoint, name='test'),
-    path('profiles/<pk>', views.ProfileViewSet.as_view({'get': 'retrieve', 'put': 'update', 'patch': 'partial_update', 'delete': 'destroy'}), name='profiles'),
-    path('', views.getRoutes),
     path('', include('djoser.urls')),
     path('', include('djoser.urls.authtoken')),
 ]
