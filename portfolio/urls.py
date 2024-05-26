b42d048a0e1b3a809665a1e0281e64fbd1c822c3
dhong9 <danielhong35@gmail.com>
2024-05-26 15:30:49 -0700

    Removed django password reset dependency

--- portfolio/urls.py
+++ portfolio/urls.py
@@ -37,7 +37,6 @@ urlpatterns = [
     path('', include(router.urls)),
     path('admin/', admin.site.urls),
     path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
-    path('password_reset/', include('django_rest_passwordreset.urls', namespace='password_reset')),
     path('accounts/', include('portfolio.accounts.urls')),
     path('comments', threadly_views.CommentViewSet.as_view({'get': 'list', 'post': 'create'}), name='comments-list'),
     path('comments/<pk>', threadly_views.CommentViewSet.as_view({'get': 'retrieve', 'put': 'update', 'patch': 'partial_update', 'delete': 'destroy'}), name='comments-detail'),
