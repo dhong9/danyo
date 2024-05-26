c6564c05fa1288a472b97881478cd6db58772f3e
dhong9 <danielhong35@gmail.com>
2024-05-26 15:03:12 -0700

    Restored ProfileViewSet

--- portfolio/accounts/views.py
+++ portfolio/accounts/views.py
@@ -1,1 +1,12 @@
+from portfolio.accounts.serializer import ProfileSerializer
+from portfolio.accounts.models import Profile
+from rest_framework import viewsets
+
 # Create your views here.
+
+class ProfileViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint that allows contacts to be viewed or edited.
+    """
+    queryset = Profile.objects.all()
+    serializer_class = ProfileSerializer
