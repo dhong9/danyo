27f5bc9e494c67b99d8108f309219df629f0a518
dhong9 <danielhong35@gmail.com>
2024-05-26 15:21:40 -0700

    Removed django password reset dependency

--- portfolio/accounts/models.py
+++ portfolio/accounts/models.py
@@ -1,7 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
 from django.dispatch import receiver
-from django_rest_passwordreset.signals import reset_password_token_created
 from django.core.mail import send_mail, EmailMessage
 from PIL import Image
 
@@ -27,25 +26,3 @@ class Profile(models.Model):
             output_size = (300, 300)
             img.thumbnail(output_size) # Resize image
             img.save(self.image.path) # Save it again and override the larger image
-
-@receiver(reset_password_token_created)
-def password_reset_token_created(sender, instance, reset_password_token, *args, **kwargs):
-
-    # the below like concatinates your websites reset password url and the reset email token which will be required at a later stage
-    email_plaintext_message = "Open the link to reset your password" + " " + "{}{}".format(instance.request.build_absolute_uri("https://danyo.tech/pages/authentication/reset-password/"), reset_password_token.key)
-    
-    """
-        this below line is the django default sending email function, 
-        takes up some parameter (title(email title), message(email body), from(email sender), to(recipient(s))
-    """
-    send_mail(
-        # title:
-        "Password Reset for {title}".format(title="Danyo Account"),
-        # message:
-        email_plaintext_message,
-        # from:
-        "noreply@danyo.tech",
-        # to:
-        [reset_password_token.user.email],
-        fail_silently=False,
-    )
