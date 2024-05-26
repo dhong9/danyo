e826705f97d54473357d98bc49fef52460d3a920
dhong9 <danielhong35@gmail.com>
2024-05-26 14:31:22 -0700

    Added DRF config

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -149,7 +149,10 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 REST_FRAMEWORK = {
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
-    'PAGE_SIZE': 10
+    'PAGE_SIZE': 10,
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.IsAuthenticated',
+    ]
 }
 
 SIMPLE_JWT = {
