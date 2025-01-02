8b65e907e459e75c4dc336e94fa2e62c96702970
dhong9 <danielhong35@gmail.com>
2025-01-01 20:01:05 -0800

    Package updates

--- portfolio/imgconverter/views.py
+++ portfolio/imgconverter/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
 from django.http import JsonResponse
+from PIL import Image
+import numpy as np
+import io
 
 def img_to_excel(request):
     return JsonResponse({"message": "Hello world!"})
