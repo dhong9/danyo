9b73c8455fd45dc2bd9d273a92a434208b98a5b1
dhong9 <danielhong35@gmail.com>
2025-01-01 17:42:33 -0800

    Referenced img_to_excel specifically

--- portfolio/imgconverter/urls.py
+++ portfolio/imgconverter/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("img_to_excel", views, name="img_to_excel")
+    path("img_to_excel", views.img_to_excel, name="img_to_excel")
 ]
