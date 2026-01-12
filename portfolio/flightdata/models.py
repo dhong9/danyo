fd338496c857f9d7f14e2fb6d837e097b442140c
dhong9 <danielhong35@gmail.com>
2026-01-11 18:14:25 -0800

    Field name fixes

--- portfolio/flightdata/models.py
+++ portfolio/flightdata/models.py
@@ -3,5 +3,5 @@ from django.db import models
 # Create your models here.
 class Airport(models.Model):
     name = models.CharField(max_length=50)
-    iata = name = models.CharField(max_length=4)
-    iata = name = models.CharField(max_length=4)
+    iata = models.CharField(max_length=4)
+    icao = models.CharField(max_length=4)
