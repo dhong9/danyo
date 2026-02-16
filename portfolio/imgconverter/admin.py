c5d3acfd17ed3ebad269aaf94ddd69f5a2a1bfeb
dhong9 <danielhong35@gmail.com>
2026-02-15 17:02:16 -0800

    Register image converter to admin

--- portfolio/imgconverter/admin.py
+++ portfolio/imgconverter/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import ImgConverter
+
+@admin.register
+class ImgConverterAdmin(admin.ModelAdmin):
+    list_display = ('date', 'image')
+    search_fields = ('date', 'image')
